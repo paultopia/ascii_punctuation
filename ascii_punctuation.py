3599e8d0f3760ed8d76d34f2c73bd78bc936068d
Paul Gowder <paul.gowder@gmail.com>
2016-01-21 23:38:12 -0600

    put the actual code in

---
+++ ascii_punctuation.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import sublime, sublime_plugin
+
+drek = {u'“': '"', u'”': '"', u"’": "'", u"‘": "'", u'—': '-', u'−': '-', u'…': '...'}
+def clean(text):
+  for key in drek.keys():
+    text = text.replace(key, drek[key])
+  return text
+
+class AsciiCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		region = sublime.Region(0, self.view.size())
+		text = self.view.substr(region)
+		self.view.replace(edit, region, clean(text))
